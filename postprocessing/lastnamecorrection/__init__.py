4deb27a8008c8d7aaec0424d061295a26ea70dfc
Daniel Segrera <segrera.d@gmail.com>
2019-01-15 20:23:10 -0700

    second commit

--- postprocessing/lastnamecorrection/__init__.py
+++ postprocessing/lastnamecorrection/__init__.py
@@ -1,11 +1,16 @@
 from postprocess_module import PostprocessModule
+import editdistance as ed
+from os.path import join
+
+THIS_DIR_PATH = 'postprocessing/dictionary'
 
 class LastNameCorrection(PostprocessModule):
 
- def __init__(self, args):
-        dict_path = args[0]
-        with open(join(THIS_DIR_PATH, dict_path)) as f:
-            self.dictionary = list(set([row for row in f.read().split('\n') if row != '']))
+    def __init__(self, args):
+        #dict_path = args[0]
+	#with open(join(THIS_DIR_PATH, dict_path)) as f:
+        #    self.dictionary = list(set([row for row in f.read().split('\n') if row != '']))
+	pass
 
     def postprocess(self, recognizer_output):
         if recognizer_output in self.dictionary:
@@ -17,6 +22,7 @@ class LastNameCorrection(PostprocessModule):
             if dist < min_ed:
                 min_ed = dist
                 best = label
-	if min_ed > (length(recognizer_output) / 2):
+	print("\n" + min_ed + " " + best + "\n")
+	if min_ed > (len(recognizer_output) / 2):
 	    return recognizer_output
         return best
