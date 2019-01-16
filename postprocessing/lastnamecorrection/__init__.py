c59e45aedabf227c99e9244fd674c90ec4e65d60
Daniel Segrera <segrera.d@gmail.com>
2019-01-16 15:43:47 -0700

    third commit

--- postprocessing/lastnamecorrection/__init__.py
+++ postprocessing/lastnamecorrection/__init__.py
@@ -2,27 +2,33 @@ from postprocess_module import PostprocessModule
 import editdistance as ed
 from os.path import join
 
-THIS_DIR_PATH = 'postprocessing/dictionary'
+THIS_DIR_PATH = 'postprocessing/lastnamecorrection'
 
 class LastNameCorrection(PostprocessModule):
 
     def __init__(self, args):
-        #dict_path = args[0]
-	#with open(join(THIS_DIR_PATH, dict_path)) as f:
-        #    self.dictionary = list(set([row for row in f.read().split('\n') if row != '']))
-	pass
+        if len(args) < 1:
+	    pass
+	else:
+	    dict_path = args[0]
+	    with open(join(THIS_DIR_PATH, dict_path)) as f:
+                self.dictionary = list(set([row for row in f.read().split('\n') if row != '']))
+	print(args)
 
     def postprocess(self, recognizer_output):
-        if recognizer_output in self.dictionary:
-            return recognizer_output
-        min_ed = float('inf')
-        best = None
-        for label in self.dictionary:
-            dist = ed.eval(label.lower(), recognizer_output.lower())
-            if dist < min_ed:
-                min_ed = dist
-                best = label
-	print("\n" + min_ed + " " + best + "\n")
-	if min_ed > (len(recognizer_output) / 2):
+	if hasattr(self, 'dictionary'): 
+            if recognizer_output in self.dictionary:
+                return recognizer_output
+            min_ed = float('inf')
+            best = None
+            for label in self.dictionary:
+                dist = ed.eval(label.lower(), recognizer_output.lower())
+                if dist < min_ed:
+                    min_ed = dist
+                    best = label
+	    print("\n" + min_ed + " " + best + "\n")
+	    if min_ed > (len(recognizer_output) / 2):
+	        return recognizer_output
+            return best
+	else:
 	    return recognizer_output
-        return best
